ea3dc151a20267e479407e101495bc58d867216c
lsouzatj <lsouza.tj@gmail.com>
2022-09-03 17:23:16 -0300

    Implementacao das validacoes JPA no model

--- src/main/java/com/ead/authUser/models/UserModel.java
+++ src/main/java/com/ead/authUser/models/UserModel.java
@@ -4,7 +4,10 @@ import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -28,15 +31,38 @@ public final class UserModel implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private UUID userId;
+	
+	@Column(nullable = false, unique = true, length = 50)
 	private String userName;
+	
+	@Column(nullable = false, unique = true, length = 50)
 	private String email;
+	
+	@Column(nullable = false, length = 10)
 	private String password;
+	
+	@Column(nullable = false, length = 150)
 	private String fullName;
+	
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
 	private UserStatus userStatus;
+	
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
 	private UserType userType;
+
+	@Column(length = 20)
 	private String phoneNumber;
+	
+	@Column(length = 20)
 	private String cpf;
+	
 	private String imageUrl;
+
+	@Column(nullable = false)
 	private LocalDateTime creationDate;
+	
+	@Column(nullable = false)
 	private LocalDateTime lastUpdateDate;
 }
