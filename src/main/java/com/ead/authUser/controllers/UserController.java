eba1f3c536ac790e733616bfee78f861aa9485ea
lsouzatj <lsouza.tj@gmail.com>
2022-09-03 14:45:05 -0300

    Criacao do servico de atualizar usuario

--- src/main/java/com/ead/authUser/controllers/UserController.java
+++ src/main/java/com/ead/authUser/controllers/UserController.java
@@ -1,5 +1,7 @@
 package com.ead.authUser.controllers;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,9 +9,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ead.authUser.dtos.UserDTO;
 import com.ead.authUser.services.UserService;
 
 @RestController
@@ -42,5 +47,24 @@ public final class UserController {
 		
 		return ResponseEntity.status(HttpStatus.OK).body(userModel);
 	}
-
+	
+	@PutMapping("/{userId}")
+	public ResponseEntity<Object> updateUser(@PathVariable(value = "userId") UUID userId,
+											 @RequestBody UserDTO userDTO){
+		
+		var userModel = userService.findById(userId);
+		
+		if (userModel.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+		}
+		
+		userModel.get().setFullName(userDTO.getFullName());
+		userModel.get().setPhoneNumber(userDTO.getPhoneNumber());
+		userModel.get().setCpf(userDTO.getCpf());
+		userModel.get().setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+		
+		userService.save(userModel.get());
+		
+		return ResponseEntity.status(HttpStatus.OK).body(userModel);
+	}
 }
