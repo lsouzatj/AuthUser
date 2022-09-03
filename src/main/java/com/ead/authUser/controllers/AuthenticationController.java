4fcba2771d7b61f9e9994ee1ed777e3b35470839
lsouzatj <lsouza.tj@gmail.com>
2022-09-03 19:07:06 -0300

    Implementacao das validacoes do Spring validation no DTO

--- src/main/java/com/ead/authUser/controllers/AuthenticationController.java
+++ src/main/java/com/ead/authUser/controllers/AuthenticationController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,7 +27,9 @@ public final class AuthenticationController {
 	UserService userService;
 	
 	@PostMapping("signup")
-	public ResponseEntity<Object> registerUser(@RequestBody UserDTO userDTO){
+	public ResponseEntity<Object> registerUser(@RequestBody 
+											   @Validated
+											   UserDTO userDTO){
 		
 		if (userService.existsUserByUserName(userDTO.getUserName())) {
 			return ResponseEntity.status(HttpStatus.CONFLICT).body("User name is exitst");
--- src/main/java/com/ead/authUser/dtos/UserDTO.java
+++ src/main/java/com/ead/authUser/dtos/UserDTO.java
@@ -2,18 +2,37 @@ package com.ead.authUser.dtos;
 
 import java.util.UUID;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 import lombok.Data;
 
 @Data
 public class UserDTO {
 
 	private UUID userId;
+
+	@NotBlank(message = "{userName.notBlank}")
+	@Size(min = 4, max = 50, message = "{userName.size}")
 	private String userName;
+	
+	@NotBlank(message = "{email.notBlank}")
+	@Email(message = "{email.format}")
 	private String email;
+
+	@NotBlank(message = "{password.notBlank}")
+	@Size(min = 4, max = 10, message = "{password.size}")
 	private String password;
+
+	@Size(min = 4, max = 10, message = "{password.size}")
 	private String oldPassword;
+	
 	private String fullName;
+	
 	private String phoneNumber;
+
 	private String cpf;
+	
 	private String imageUrl;
 }
